cbd7d94f7529b90e589b3f8704d9c28dd023c8cc
Andrew Greenwell <agreenwell@collegeloan.com>
2017-10-16 14:07:38 -0700

    .gitignore update

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModuleNgFactory } from '../aot/src/app/app.module.ngfactory';
-console.log('Running AOT compiled');
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
-//# sourceMappingURL=main.js.map
