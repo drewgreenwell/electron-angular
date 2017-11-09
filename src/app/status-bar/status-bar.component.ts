43cc8bde7b88be3b6ff1abbed6e46042c88c943b
Andrew Greenwell <agreenwell@collegeloan.com>
2017-11-09 15:52:33 -0800

    changed to scss
    removed ViewEncapsulation

--- src/app/status-bar/status-bar.component.ts
+++ src/app/status-bar/status-bar.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-status-bar',
     templateUrl: './status-bar.component.html',
-    styleUrls: ['./status-bar.component.scss']
+    styleUrls: ['./status-bar.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class StatusBarComponent implements OnInit {
     constructor() { }
